2142d562fd4f4895dd3dbba7b345667749b9038e
Suraj Kumar <surajgautam56878@gmail.com>
2026-02-08 20:24:15 +0530

    add:frontend setuo

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-600">
+        Tailwind v4 is working 🚀
+      </h1>
+    </div>
+  );
+}
+
